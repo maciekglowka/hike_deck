a1d2d105e206c99c9602e8ea71fc407cc901c8cc
maciek.glowka <maciek.glowka@gmail.com>
2023-04-12 23:29:54 +0200

    piece components

--- src/pieces/components.rs
+++ src/pieces/components.rs
@@ -3,7 +3,22 @@ use bevy::prelude::*;
 use crate::actions::Action;
 
 #[derive(Component, Default)]
-pub struct Actor(pub Option<Box<dyn Action>>);
+pub struct Actor(pub Vec<(Box<dyn Action>, i32)>);
+
+#[derive(Component)]
+pub struct Health {
+    pub value: u32
+}
+
+#[derive(Component)]
+pub struct Melee {
+// melee attack behaviour for the npcs
+    pub damage: u32
+}
+
+#[derive(Component)]
+// there can be only a single occupier piece on the same tile
+pub struct Occupier;
 
 #[derive(Component)]
 pub struct Piece {
