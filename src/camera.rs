bc77f75ed3ad07d5b928dacc481bb05133d7b809
maciek.glowka <maciek.glowka@gmail.com>
2023-06-09 21:01:52 +0200

    room generator

--- src/camera.rs
+++ src/camera.rs
@@ -5,10 +5,10 @@ use crate::graphics::TILE_SIZE;
 pub fn setup(mut commands: Commands) {
     let mut camera = Camera2dBundle::default();
     // temporary ;)
-    camera.projection.scale = 2.;
+    camera.projection.scale = 4.;
     camera.transform.translation = Vec3::new(
+        40. * TILE_SIZE,
         20. * TILE_SIZE,
-        10. * TILE_SIZE,
         camera.transform.translation.z
     );
     commands.spawn(camera);
